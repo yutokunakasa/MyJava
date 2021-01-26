8f66471cc60a09d1a1e857c187115e226b3d8726
仲佐　祐德 <yutokunakasa@nakatasukuyuunoMacBook-Air.local>
2021-01-26 13:32:06 +0900

    [finish]Lesson10

---
+++ Lesson09.java
@@ -0,0 +1,49 @@
+import java.time.Year;
+
+class Lesson09 {
+    public static void main(String[] args) {
+        // int x = 10;
+        // int y = 2;
+        // int z = 10;
+
+        // System.out.println(x+y);
+        // System.out.println(x-y);
+        // System.out.println(x*y);
+        // System.out.println(x/y);
+        // System.out.println(x%y);
+
+        // System.out.println(x < y);
+        // System.out.println(x >= y);
+        // System.out.println(x <= y);
+        // System.out.println(x <= z);
+
+        // System.out.println(x == y);
+        // System.out.println(x != y);
+
+        // int x = 8;
+        // int y = 3;
+
+        // System.out.println(x >= 5 && x<= 10);
+        // System.out.println(y >= 5 && y <=10);
+
+        // System.out.println(x == 3 || y == 3);
+        // System.out.println(x == 1 || y == 1);
+
+        // int x = 8;
+        // int y = 12;
+        // int z = 20;
+
+        // System.out.println(x += 10);
+        // System.out.println(z += y);
+
+        int x = 8;
+        int y = 8;
+        x++;
+        y--;
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
---
+++ Lesson10.java
@@ -0,0 +1,14 @@
+class Lesson10 {
+    public static void main(String[] args) {
+        int age = 0;
+
+        if (age >= 20) {
+            System.out.println("adult");    
+        }else if(age==0){
+            System.out.println("baby");
+        }else {
+            System.out.println("child");
+        }
+    }
+    
+}
